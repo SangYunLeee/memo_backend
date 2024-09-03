8c00816f3a75ba4b15908693b962a5d7afb23aa2
sangyun lee <sororiri@gmail.com>
2024-09-04 04:17:56 +0900

    root path 를 Public 으로 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './common/decorator/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
+  @IsPublic()
   getHello(): string {
     return this.appService.getHello();
   }
