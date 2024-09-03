5b350aa92667fb8eb0d13ffcb9ca5a7166a042d9
sangyun lee <sororiri@gmail.com>
2024-09-04 03:20:38 +0900

    feat: Validate Pipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.SERVER_PORT || 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
